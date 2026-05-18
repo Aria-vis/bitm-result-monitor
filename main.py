570460ae29dd58a6b77eae7cc09c48b6e47ebb2c
Aria-vis <swapnilarya123456789@gmail.com>
2026-05-18 17:37:44 +0530

    Add ERP tab detection and navigation handling

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
 
-    page = browser.new_page()
+    context = browser.new_context()
+
+    page = context.new_page()
 
     page.goto("https://www.bitmesra.ac.in/")
 
-    print("Website opened successfully.")
+    print("BIT Mesra website opened.")
+    print("Click ERP manually...")
 
-    input("Press Enter to close browser...")
+    # Wait for new ERP tab
+    erp_page = context.wait_for_event("page")
+
+    print("\nERP tab detected.")
+
+    erp_page.wait_for_load_state()
+
+    print("\nERP URL:")
+    print(erp_page.url)
+
+    input("\nPress Enter to close browser...")
 
     browser.close()
